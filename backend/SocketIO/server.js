3548cae77dd97c9cc3eaef3c9606b4923cf7bef5
Aditya Shashank <adi620321@gmail.com>
2025-05-24 01:41:30 +0530

    handling in frontend; update backend routes and socket setup

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -52,11 +52,57 @@ function setupSocketIO(app) {
     });
   });
 
-  return { server, io };
+  return { server };
 }
 
 const getReceiverSocketId = (receiverId) => {
-  return io ? io.sockets?.sockets?.get(receiverId) : null;
+
+
+  if (!io) return null;
+
+  return users[receiverId] || null;
 };
-z
+
+const users = {};
+
+function setupSocketIO(app) {
+  const server = http.createServer(app);
+
+  io = new Server(server, {
+    cors: {
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        } else {
+          return callback(new Error("Socket.IO CORS policy violation"), false);
+        }
+      },
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+      users[userId] = socket.id;
+      console.log("Connected users:", users);
+    }
+
+    io.emit("getOnlineUsers", Object.keys(users));
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected", socket.id);
+      if (userId) {
+        delete users[userId];
+      }
+      io.emit("getOnlineUsers", Object.keys(users));
+    });
+  });
+
+  return { server };
+}
+
 export { setupSocketIO, getReceiverSocketId };
--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,34 +1,41 @@
-import { getReceiverSocketId, io } from "../SocketIO/server.js";
+import { getReceiverSocketId } from "../SocketIO/server.js";
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
     const { id: receiverId } = req.params;
     const senderId = req.user._id; // current logged in user
+
     let conversation = await Conversation.findOne({
       members: { $all: [senderId, receiverId] },
     });
+
     if (!conversation) {
       conversation = await Conversation.create({
         members: [senderId, receiverId],
       });
     }
+
     const newMessage = new Message({
       senderId,
       receiverId,
       message,
     });
+
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
-    // await conversation.save()
-    // await newMessage.save();
-    await Promise.all([conversation.save(), newMessage.save()]); // run parallel
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
     const receiverSocketId = getReceiverSocketId(receiverId);
     if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
+
+      console.log("Would emit 'newMessage' to socket id:", receiverSocketId);
     }
+
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage", error);
@@ -40,12 +47,15 @@ export const getMessage = async (req, res) => {
   try {
     const { id: chatUser } = req.params;
     const senderId = req.user._id; // current logged in user
+
     let conversation = await Conversation.findOne({
       members: { $all: [senderId, chatUser] },
     }).populate("messages");
+
     if (!conversation) {
       return res.status(201).json([]);
     }
+
     const messages = conversation.messages;
     res.status(201).json(messages);
   } catch (error) {
