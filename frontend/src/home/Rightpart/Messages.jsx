f94f0b5f5224bf6ee10a4fe36cfe9af2fa90859e
Aditya Shashank <adi620321@gmail.com>
2025-05-24 02:30:37 +0530

    message

--- frontend/src/home/Rightpart/Messages.jsx
+++ frontend/src/home/Rightpart/Messages.jsx
@@ -19,30 +19,27 @@ function Messages() {
     }, 100);
   }, [messages]);
   return (
-    <div
-      className="flex-1 overflow-y-auto"
-      style={{ minHeight: "calc(92vh - 8vh)" }}
-    >
-      {loading ? (
-        <Loading />
-      ) : (
-        messages.length > 0 &&
-        messages.map((message) => (
-          <div key={message._id} ref={lastMsgRef}>
-            <Message message={message} />
-          </div>
-        ))
-      )}
-
-      {!loading && messages.length === 0 && (
-        <div>
-          <p className="text-center mt-[20%]">
-            Say! Hi to start the conversation
-          </p>
+  <div
+    className="flex-1 overflow-y-auto"
+    style={{ minHeight: "calc(92vh - 8vh)" }}
+  >
+    {loading ? (
+      <Loading />
+    ) : Array.isArray(messages) && messages.length > 0 ? (
+      messages.map((message) => (
+        <div key={message._id} ref={lastMsgRef}>
+          <Message message={message} />
         </div>
-      )}
-    </div>
-  );
+      ))
+    ) : (
+      <div>
+        <p className="text-center mt-[20%]">
+          Say! Hi to start the conversation
+        </p>
+      </div>
+    )}
+  </div>
+);
 }
 
 export default Messages;
