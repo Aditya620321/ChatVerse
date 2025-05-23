c7bfd71826eeca138aa0050732e756cfefd67f60
Aditya Shashank <adi620321@gmail.com>
2025-05-24 03:29:16 +0530

    signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useAuth } from "../context/AuthProvider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 function Signup() {
   const [authUser, setAuthUser] = useAuth();
