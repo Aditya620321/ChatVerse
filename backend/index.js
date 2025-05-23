e2c8b73e465f937555790c52bcea4a2e20ce02fe
Aditya Shashank <adi620321@gmail.com>
2025-05-23 12:49:05 +0530

    Fix index.js

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,17 @@ import cookieParser from "cookie-parser";
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
-import { app, server } from "./SocketIO/server.js"; 
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
 
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-const cors = require("cors");
-
 app.use(cors({
   origin: "https://chatverse-frontend.onrender.com",
   credentials: true,
 }));
-
 
 const PORT = process.env.PORT || 5002;
 const URI = process.env.MONGODB_URI;
@@ -28,16 +25,14 @@ mongoose.connect(URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
-
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-
-//routes
+// routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
 server.listen(PORT, () => {
-    console.log(`Server is Running on port ${PORT}`);
+  console.log(`Server is Running on port ${PORT}`);
 });
